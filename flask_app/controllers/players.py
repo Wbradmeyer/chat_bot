9263db52825126432b500d7187e25bf2e9cf9dc8
W. Brad Meyer <wbradmeyer@gmail.com>
2024-06-10 23:17:59 -0400

    controller route for create player

--- flask_app/controllers/players.py
+++ flask_app/controllers/players.py
@@ -1,3 +1,12 @@
 from flask_app import app
 from flask import render_template, redirect, request, session
 from flask_app.models import player
+
+@app.route('/players/new', methods=['POST', 'GET'])
+def create_player():
+    if request.method == 'POST':
+        # create a new instrument and retrieve the id
+        player_id = player.Player.create_new_player(request.form)
+        if player_id:
+            return redirect('/home')
+    return render_template('create_instrument.html', data = request.form)
