eb3f734196a6d514f2364979f3fd574582337b20
W. Brad Meyer <wbradmeyer@gmail.com>
2024-10-18 23:41:07 -0400

    remove extra import

--- flask_app/controllers/players.py
+++ flask_app/controllers/players.py
@@ -2,7 +2,6 @@ from flask_app import app
 from flask import render_template, redirect, request, session
 from flask_app.models import player
 import openai
-# import requests
 
 @app.route('/players/new', methods=['POST', 'GET'])
 def create_player():
